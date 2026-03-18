cb4cf86113b8b4544ced6ac3f03ef54c4ee4b1a6
aabccd021 <aabccd021@gmail.com>
2026-03-18 15:47:35 +0700

    Update treefmt config with deadnix and shellcheck

--- flake.nix
+++ flake.nix
@@ -1,27 +1,39 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
 
   outputs =
-    { nixpkgs, treefmt-nix, ... }:
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+      ...
+    }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
       treefmtEval = treefmt-nix.lib.evalModule pkgs {
-        projectRootFile = "flake.nix";
+        programs.deadnix.enable = true;
         programs.nixfmt.enable = true;
         programs.shfmt.enable = true;
+        programs.shellcheck.enable = true;
+      };
+
+      formatter = treefmtEval.config.build.wrapper;
+
+      packages = {
+        default = pkgs.writeShellApplication {
+          name = "age-rekey";
+          runtimeInputs = [ ];
+          text = builtins.readFile ./age-rekey.sh;
+        };
+        formatting = treefmtEval.config.build.check self;
+        formatter = formatter;
       };
     in
     {
-      formatter.${system} = treefmtEval.config.build.wrapper;
-      checks.${system}.formatting = treefmtEval.config.build.check ./.;
-      packages.${system}.default = pkgs.writeShellApplication {
-        name = "age-rekey";
-        runtimeInputs = [ ];
-        text = builtins.readFile ./age-rekey.sh;
-      };
+      packages.x86_64-linux = packages;
+      checks.x86_64-linux = packages;
+      formatter.x86_64-linux = formatter;
     };
 }
